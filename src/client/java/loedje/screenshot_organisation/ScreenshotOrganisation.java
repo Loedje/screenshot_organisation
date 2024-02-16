451a50c9b90574546ca6b9233ec661c9eab9b018
Robin Jan <loedje5@gmail.com>
2024-02-16 15:04:21 +0100

    now should be compatible with shared resources

--- src/client/java/loedje/screenshot_organisation/ScreenshotOrganisation.java
+++ src/client/java/loedje/screenshot_organisation/ScreenshotOrganisation.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ClientModInitializer;
 
 public class ScreenshotOrganisation implements ClientModInitializer {
 	public static final Config CONFIG = new Config();
+
 	/**
 	 * Runs the mod initializer on the client environment.
 	 */
--- src/client/java/loedje/screenshot_organisation/mixin/client/ScreenshotFolder.java
+++ src/client/java/loedje/screenshot_organisation/mixin/client/ScreenshotFolder.java
@@ -1,45 +1,63 @@
 package loedje.screenshot_organisation.mixin.client;
 
 import loedje.screenshot_organisation.ScreenshotOrganisation;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ServerInfo;
 import net.minecraft.client.util.ScreenshotRecorder;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.WorldSavePath;
+import nl.enjarai.shared_resources.api.GameResourceHelper;
+import nl.enjarai.shared_resources.api.GameResourceRegistry;
+import nl.enjarai.shared_resources.api.ResourceDirectory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.util.Map;
 
-@Mixin(ScreenshotRecorder.class)
+@Mixin(value = ScreenshotRecorder.class, priority = 1200)
 public class ScreenshotFolder {
 
 	@ModifyVariable(method = "saveScreenshotInner", at = @At("STORE"), ordinal = 1)
 	private static File injected(File file) {
 		MinecraftClient client = MinecraftClient.getInstance();
 		Map<String, String> rules = ScreenshotOrganisation.CONFIG.getRules();
-		File screenshotsDir = new File(client.runDirectory, ScreenshotRecorder.SCREENSHOTS_DIRECTORY);
+		// Shared resources mod
 
-		if (client.getServer() != null) {
+		File screenshotsDir = new File(client.runDirectory,
+				ScreenshotRecorder.SCREENSHOTS_DIRECTORY);
+
+		// IDK if this affects performance when you take a screenie
+		if (FabricLoader.getInstance().isModLoaded("shared-resources")) {
+			ResourceDirectory resourceDirectory = ((ResourceDirectory) GameResourceRegistry.REGISTRY.get(new Identifier("shared-resources:screenshots")));
+			Path path = GameResourceHelper.getPathFor(resourceDirectory);
+			if (path != null) {
+				screenshotsDir = path.toFile();
+			}
+		}
+
+		if (client.getServer() != null) { // Single player
 			String location = client.getServer().getSavePath(WorldSavePath.ROOT).toString();
 			location = location.substring(0, location.length() - 2);
-			if (rules.containsKey(location)) {
+			if (rules.containsKey(location)) { // From config
 				return new File(rules.get(location));
-			} else {
+			} else { // No config
 				String levelName = client.getServer().getSaveProperties().getLevelName();
 				return new File(screenshotsDir, levelName.replaceAll("[\\\\/:*?\"<>|]", "_"));
 			}
-		} else if (client.getNetworkHandler() != null) {
+		} else if (client.getNetworkHandler() != null) { // Multiplayer
 			ServerInfo serverInfo = client.getNetworkHandler().getServerInfo();
 			String location = serverInfo.address;
-			if (rules.containsKey(location)) {
+			if (rules.containsKey(location)) { // From config
 				return new File(rules.get(location));
-			} else {
+			} else { // No config
 				return new File(screenshotsDir, (location + " - " + serverInfo.name)
 						.replaceAll("[\\\\/:*?\"<>|]", "_"));
 			}
-		} else {
+		} else { // Screenshot outside a world.
 			return file;
 		}
 	}
